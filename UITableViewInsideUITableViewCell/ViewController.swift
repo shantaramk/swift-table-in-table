7271b12afbb872433131a62848dc90489f7fe483
Shantaram Kokate <shantakokateit@gmail.com>
2020-10-02 00:52:07 +0530

    Initial Commit

---
+++ UITableViewInsideUITableViewCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewInsideUITableViewCell
+//
+//  Created by Shantaram K on 02/10/20.
+//  Copyright © 2020 Shantaram Kokate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
